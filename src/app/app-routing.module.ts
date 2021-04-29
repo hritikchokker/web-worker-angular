e4a9f15c1d57ac73346047736df20ce17e9179c9
hritikchokker <hchokker7@gmail.com>
2021-04-29 22:50:09 +0530

    worker thread example added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 const routes: Routes = [
-  {
-    path: 'list',
-    loadChildren: () => import('./dummy-list/dummy-list.module').then(m => m.DummyListModule)
-  }
 ];
 
 @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'web-worker-angular';
+  output: any;
+  webWorker!: Worker;
+  sameThreadOutput!: number;
+  ngOnInit(): void {
+    if (typeof Worker !== 'undefined') {
+      this.webWorker = new Worker('./app.worker', { type: 'module' });
+      this.webWorker.onmessage = ({ data }) => {
+        this.output = data;
+        console.log(`page got message: ${data}`);
+      };
+    }
+  }
+  calcFib(num: number): void {
+    this.webWorker.postMessage({ num });
+  }
+  fibonacci(num: number): number {
+    if (num === 1 || num === 2) {
+      return 1;
+    }
+    return this.fibonacci(num - 1) + this.fibonacci(num - 2);
+  }
+  fibonacSameThread(num: number): void {
+    const result = this.fibonacci(num);
+    this.sameThreadOutput = result;
+  }
+
+
 }
